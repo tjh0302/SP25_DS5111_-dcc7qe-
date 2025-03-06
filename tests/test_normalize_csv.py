4fe4cf1f58de1d53a84067f71be7465f0fa94ff4
Efrain Olivares <99214069+EfrainOlivaresUVA@users.noreply.github.com>
2025-03-05 21:18:46 -0800

    Update test_normalize_csv.py

--- tests/test_normalize_csv.py
+++ tests/test_normalize_csv.py
@@ -1,5 +1,7 @@
 '''
 This module tests the other files in this repository.
+# apply 6 month rule here.  If there were ten files and you opened one of the test files does the liner help?
+# in this case something like `Test that we convert first pandas generated csv to known standard` would be better I think
 '''
 import sys
 import pandas as pd
@@ -21,3 +23,5 @@ def test_normalize_wsj():
     '''
     assert isinstance(normal.wsj_norm, pd.DataFrame), "Expected a pandas dataframe"
     assert len(normal.wsj_norm.columns) == 4, "wsj_norm has the incorrect number of columns."
+
+# can't find the tests for OS and python version
