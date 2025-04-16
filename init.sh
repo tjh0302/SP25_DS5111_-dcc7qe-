0ded6759c3fabf95a0d962478e6494aca609f9ff
tjh0302 <tjhinnendael@gmail.com>
2025-04-16 00:16:23 +0000

    This commit includes the data collected over the 5 days for lab 7

--- init.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update
-sudo apt install make -y
-sudo apt install python3.12-venv -y
-sudo apt install tree
--- tests/test_python_version.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-def test_python_version():
-    version = sys.version_info
-    assert version >= (3, 10), "Python version needs to be 3.10 or 3.11"
-    assert version < (3, 12), "Python version needs to be 3.10 or 3.11"
